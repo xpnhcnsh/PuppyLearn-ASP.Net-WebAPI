f285ff76e398cd78657322db9c919bb848489a6d
HanchenNan <xpnhcnsh@gmail.com>
2024-02-11 18:14:01 +0800

    321

--- Models/Dto/LoginDto.cs
+++ Models/Dto/LoginDto.cs
@@ -3,6 +3,6 @@
     public class LoginDto
     {
         public string Password { get; set; } = null!;
-        public string Email { get; set; } = null!;
+        public string Email { get; set; }
     }
 }
